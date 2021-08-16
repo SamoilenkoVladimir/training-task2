758a0bb9e88f637a99c7e213593b49ee9b711c76
Vladimir Samoilenko <81811148+SamoilenkoVladimir@users.noreply.github.com>
2021-08-16 11:47:32 +0300

    Update Sentence.java

--- src/Sentence.java
+++ src/Sentence.java
@@ -1,1 +1,24 @@
 
+
+public class Sentence {
+private String s;
+
+public Sentence(String s) {
+	super();
+	this.s = s;
+}
+
+public String getS() {
+	return s;
+}
+
+public void setS(String s) {
+	this.s = s;
+}
+
+
+
+
+
+}
+
