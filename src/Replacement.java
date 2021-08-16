95d04188e5d72f027013bf75dbcff8a6052983e8
Vladimir Samoilenko <81811148+SamoilenkoVladimir@users.noreply.github.com>
2021-08-16 11:49:57 +0300

    Update Replacement.java

--- src/Replacement.java
+++ src/Replacement.java
@@ -1,1 +1,25 @@
 
+
+public class Replacement {
+	
+	public static String wordReplace(String arrString,String r,Integer length) {
+		//замена слов заданной длины на последовательность символов (при этом строка разбивается на слова)
+		arrString=" "+arrString+" ";
+		r=" "+r+" ";
+		String newString[]=arrString.split(" ");
+		arrString="";
+        for (String string : newString) {
+        	string=" "+string+" ";
+            string=string.replaceAll(String.format("\\s\\S{%d}\\s+", length), r);
+            arrString=arrString+string;
+           // System.out.println("after ="+string);
+        }
+        arrString=" "+arrString+" ";
+        arrString = arrString.replaceAll("\\p{Blank}+", " ");
+		
+		
+		return arrString.substring(1, arrString.length()-1);
+	}
+
+}
+
